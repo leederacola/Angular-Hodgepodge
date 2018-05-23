96a13badbe0549858035ab2c8e65c2a4cc4a1f3e
Ryan <Ryan@CROCODILE>
2018-05-23 10:41:28 -0400

    router and home component

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule }             from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+
+import { HomeComponent } from './Components/home/home.component';
+
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent }
+];
+
+
+@NgModule({
+  imports: [
+     RouterModule.forRoot(routes)
+  ],
+  declarations: []
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule }    from '@angular/forms';
 import { AppComponent } from './app.component';
 
-import { FormsModule } from '@angular/forms';
-
+import { RouterModule } from '@angular/router';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HeaderComponent } from './Components/header/header.component';
+import { AppRoutingModule } from './/app-routing.module';
+import { HomeComponent } from './Components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     NgbModule.forRoot(),
+    AppRoutingModule,
+    RouterModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
